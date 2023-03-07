a4501c94f83ba221454a39473694162ecf4e508e
Daniel Hollander <daniel.hollander@appendad.com>
2023-03-07 07:50:53 +0200

    changed tests texts and added an example video

--- readme.md
+++ readme.md
@@ -4,7 +4,9 @@ This app is an exercise for opus security intended to test the entities API
 ## Installation
 
 ### To install the project clone the repository
-
+```bash
+https://github.com/dhollander8991/opus.git
+```
 
 ### Then run 
 ```bash
@@ -18,3 +20,5 @@ run the following command:
 ```bash
 testcafe chrome:headless suite.js
 ```
+## Example
+Check out the video in the assets folder 
--- test-suite/suite.js
+++ test-suite/suite.js
@@ -4,19 +4,19 @@ const testcases = require("./testcases/testcase.js");
 
 fixture(`Testing entities manager API`)
 
-testcases.forEach((ele) => {
-    test('Creating entity', async t => {
+testcases.forEach((ele, index) => {
+    test(`Creating entity for test number ${index + 1}`, async t => {
         const res = await utils.setEntity();
         await t.expect(res).ok(`Couldn't set entity on server - check log to see what went wrong`);
         ele = res.data.id;
     });
 
-    test('Checking entity exists by id', async t => {
+    test(`Checking entity exists by id for test number ${index + 1}`, async t => {
         const res = await utils.getEntityById(ele);
         await t.expect(res).ok(`Couldn't get entity from server - check log to see what went wrong`);
     });
 
-    test('Checking Entity exists in all entities', async t => {
+    test(`Checking entity exists in all entities for test number ${index + 1}`, async t => {
         const res = await utils.getEntityById();
         await t.expect(res.data.find((e) => e.id == ele)).ok(`Couldn't get entities from server - check log to see what went wrong`);
     });
